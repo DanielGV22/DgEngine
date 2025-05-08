6ba294af906069d86aabe994757fbdc394060421
Daniel Adrian Gonzalez Valencia <daniel_adrian.gonzalez_valencia@lcieducation.net>
2025-05-08 01:00:11 -0500

    Week 2 Hello Window Math lib

--- Engine/DgEngine/Inc/DgEngine.h
+++ Engine/DgEngine/Inc/DgEngine.h
@@ -1,3 +1,12 @@
 #pragma once
 
 #include"Common.h"
+
+#include"App.h"
+#include"AppState.h"
+
+namespace DgEngine
+{
+	App& MainApp();
+
+}
---
+++ Engine/DgEngine/Src/App.cpp
@@ -0,0 +1,72 @@
+#include"Precompiled.h"
+#include"App.h"
+#include"AppState.h"
+
+using namespace DgEngine;
+using namespace DgEngine::Core;
+
+void App::Run(const AppConfig& config)
+{
+	LOG("App Started");
+
+	Window myWindow;
+	myWindow.Initialize(
+		GetModuleHandle(nullptr),
+		config.appName,
+		config.winWidth,
+	    config.winHeight
+	);
+
+
+	// last step before running
+	ASSERT(mCurrentState != nullptr, "App: need an app state to run");
+	mCurrentState->Initialize();
+
+	// Process updates
+	mRunning = true;
+	while (mRunning) 
+	{
+		myWindow.ProcessMessage();
+		if (!myWindow.IsActive()) 
+		{
+			Quit();
+			continue;
+		}
+
+		if(mNextState != nullptr)
+		{
+			mCurrentState->Terminate();
+			mCurrentState = std::exchange(mNextState, nullptr);
+			mCurrentState->Initialize();
+		}
+
+		float deltaTime = TimeUtil::GetDeltaTime();
+ #if defined(_DEBUG)
+		if (deltaTime < 0.5f)// primarily for handling breakpoints 
+#endif
+		{
+			mCurrentState->Update(deltaTime);
+		}
+	}
+
+	// Terminate everything
+	LOG("App Quit");
+	mCurrentState->Terminate();
+
+	myWindow.Terminate();
+}
+
+void App::Quit()
+{
+	mRunning = false;
+
+}
+
+void App::ChangeState(const std::string& stateName)
+{
+	auto iter = mAppStates.find(stateName);
+	if(iter != mAppStates.end())
+	{
+		mNextState = iter->second.get();
+	}
+}
