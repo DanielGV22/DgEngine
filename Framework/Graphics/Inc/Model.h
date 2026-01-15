a3517738a87c25537b67cfcd918eadb7380008bb
Daniel Adrian Gonzalez Valencia <daniel_adrian.gonzalez_valencia@lcieducation.net>
2026-01-14 23:55:51 -0600

    Bone/Skeleton initial implementation

--- Framework/Graphics/Inc/Model.h
+++ Framework/Graphics/Inc/Model.h
@@ -2,6 +2,8 @@
 
 #include "MeshTypes.h"
 #include "Material.h"
+#include "Skeleton.h"
+
 
 namespace DgEngine::Graphics
 {
@@ -25,5 +27,6 @@ namespace DgEngine::Graphics
 
 		std::vector<MeshData> meshData;
 		std::vector<MaterialData> materialData;
+		std::unique_ptr<Skeleton> skeleton;
 	};
 }
