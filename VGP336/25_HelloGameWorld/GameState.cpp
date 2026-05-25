64e8614a324a34a81317a5691d92cf553bbd2202
Daniel Adrian Gonzalez Valencia <daniel_adrian.gonzalez_valencia@lcieducation.net>
2026-05-24 22:41:14 -0500

    Level 2 & 3

--- VGP336/25_HelloGameWorld/GameState.cpp
+++ VGP336/25_HelloGameWorld/GameState.cpp
@@ -38,7 +38,7 @@ Component* GetCustomComponent(const std::string& componentName, GameObject& game
 
 void GameState::Initialize()
 {
-	mLevelFile = L"../../Assets/Templates/Levels/level.json";
+	mLevelFile = L"../../Assets/Templates/Levels/level_2.json";
 
 	// set a callback to try make a custom service (any service that is NoT part of the engine and unique to the project)
 	GameWorld::SetCustomService(MakeCustomService);
